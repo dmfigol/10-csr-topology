a3326faac7d43f36cac69c6344dcdf362fb609ba
Dmitry Figol <git@dmfigol.me>
2020-01-26 04:02:38 +0100

    Refactor restconf check script

--- restconf-check.sh
+++ restconf-check.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env sh
-curl -k -u cisco:cisco -H "Accept: application/yang-data+json" https://198.18.1.101/restconf/data/native/hostname
-curl -k -u cisco:cisco -H "Accept: application/yang-data+json" https://198.18.1.102/restconf/data/native/hostname
-curl -k -u cisco:cisco -H "Accept: application/yang-data+json" https://198.18.1.103/restconf/data/native/hostname
-curl -k -u cisco:cisco -H "Accept: application/yang-data+json" https://198.18.1.104/restconf/data/native/hostname
-curl -k -u cisco:cisco -H "Accept: application/yang-data+json" https://198.18.1.105/restconf/data/native/hostname
-curl -k -u cisco:cisco -H "Accept: application/yang-data+json" https://198.18.1.106/restconf/data/native/hostname
-curl -k -u cisco:cisco -H "Accept: application/yang-data+json" https://198.18.1.107/restconf/data/native/hostname
-curl -k -u cisco:cisco -H "Accept: application/yang-data+json" https://198.18.1.108/restconf/data/native/hostname
-curl -k -u cisco:cisco -H "Accept: application/yang-data+json" https://198.18.1.109/restconf/data/native/hostname
-curl -k -u cisco:cisco -H "Accept: application/yang-data+json" https://198.18.1.110/restconf/data/native/hostname
+for octet in 101 102 103 104 105 106 107 108 109 110; do \
+  curl -k -u cisco:cisco -H "Accept: application/yang-data+json" https://198.18.1.$octet/restconf/data/native/hostname
+done
