c63869593187c6832c19724d3b0d1b015cf01b66
Dmitry Figol <git@dmfigol.me>
2020-02-27 13:44:08 +0000

    Revert "Change network in RESTCONF script"
    This reverts commit 3d1fcf35f3e737964882d06e4bd9dbe93bc4fb46.

--- restconf-check.sh
+++ restconf-check.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 for octet in 101 102 103 104 105 106 107 108 109 110; do \
-  curl -k -u cisco:cisco -H "Accept: application/yang-data+json" https://192.168.153.$octet/restconf/data/native/hostname
+  curl -k -u cisco:cisco -H "Accept: application/yang-data+json" https://198.18.1.$octet/restconf/data/native/hostname
 done
